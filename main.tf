4c41dedf7258c081e3a10415e92aab3d5673aafa
Nehal Ingole <73941735+Ingole712521@users.noreply.github.com>
2024-06-07 14:23:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,63 @@
-provider "github" {
-  token = var.github_token
-  owner = var.github_owner
+provider "aws" {
+  region = var.aws_region
 }
 
-resource "github_repository" "candy_crush_docker_repo" {
-  name        = "Candy-crush-Docker"
-  description = "Repository for Candy Crush Docker setup"
-  visibility  = "public"
+resource "aws_ecr_repository" "candy_crush_repo" {
+  name = var.ecr_repository_name
 }
 
-resource "github_actions_secret" "example_secret" {
-  repository     = github_repository.candy_crush_docker_repo.name
-  secret_name    = "MY_SECRET"
- plaintext_value = "my_secret_value"
+resource "aws_ecs_cluster" "candy_crush_cluster" {
+  name = var.ecs_cluster_name
+}
+
+resource "aws_ecs_task_definition" "candy_crush_task" {
+  family                   = var.ecs_task_family
+  container_definitions    = file(var.container_definitions_file)
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  memory                   = var.ecs_task_memory
+  cpu                      = var.ecs_task_cpu
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_execution_role.arn
+}
+
+resource "aws_ecs_service" "candy_crush_service" {
+  name            = var.ecs_service_name
+  cluster         = aws_ecs_cluster.candy_crush_cluster.id
+  task_definition = aws_ecs_task_definition.candy_crush_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = var.subnet_ids
+    security_groups  = var.security_group_ids
+    assign_public_ip = true
+  }
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
